c2ee9e0878ad482317706cfad54e43be29bcb61f
Madhulika mishra <madhu9@users.noreply.github.com>
2020-10-30 21:47:01 +0000

    Initial commit

---
+++ R/Batch_correction_methods.R
@@ -0,0 +1,212 @@
+#' Correction of batch effect in merged dataset
+#' @description  This function performs various type of batch-correction on given merged
+#' dataset and output batch-corrected data as a list.
+#' @param experiment It is the merged dataset obtained at the previous step (`merge_experiments`).
+#' @param model A R formula mentioning the biological factor to take into account during the correction.
+#' @param k No. of confounders for RUV methods and denotes the "number of factors of unwanted variation to remove".
+#' If k is not provided it will then estimated from input data.
+#' @param batch meta-data column which has information about batch-label
+#' @param filter A string. Should be one of following string- 'symbol', 'ensembl_gene_id', or 'entrezgene_id'
+#' depending on gene label for the given dataset.
+#' @return A list of the batch-corrected experiment.
+#' @details  This function contains a cocktail of various batch-correction methods.
+#'
+#' For Microarray, `batch_correction` function has following methods-
+#'
+#'a) limma- Default limma setting,
+#'b) GFS - Gene Fuzzy Score method.
+#'c) Robust Quantile Normalization- quantile normalization method from PreprocessCore package,
+#'d) ComBat- Implemented from SVA package,
+#'e) Q_ComBat - It is Quantile+ parametric adjustment of ComBat,
+#'f) MNN - is default mnnCorrect function from batchelor package.
+#'g) naiveRandRUV_HK - default naiveRandRUV using Human HK genes from RUVnormalize package,
+#'h) Q_naiveRandRUV_HK - is Qunatile+ naiveRandRUV_HK,
+#'i) naiveRandRUV_empi.controls- default naiveRandRUV using empirically derived HK genes from RUVnormalize package,
+#'j) Q_naiveRandRUV_empi.controls - is Qunatile+ naiveRandRUV_empi.controls.
+#'
+#' For bulk-RNAseq, `batch_correction` function has following methods-
+#'
+#' a) limma- default limma setting,
+#' b) ComBat- implemented from SVA package,
+#' c) ComBat_seq - implemented from ComBat-Seq package,
+#' d) MNN- default mnnCorrect function from batchelor package,
+#' e) RUVs- default RUVs function from RUVSeq package,
+#' g) scBatch- batch correction method implemented in scBatch package.
+
+#'
+#' @import sva
+#' @import RUVnormalize
+#' @import limma
+#' @import WGCNA
+#' @import preprocessCore
+#' @import Biobase
+#' @import RUVSeq
+#' @import batchelor
+#' @import SummarizedExperiment
+#' @import scBatch
+#' @import edgeR
+
+#' @include gfs_function.R
+#' @include ComBat_seq.R
+#' @include helper_ComBat_seq.R
+#' @include Metadatacreation.R
+
+#' @name batch_correction
+
+data(control.genes, envir=environment())
+#' @export
+batch_correction <-function(experiment, model,k=NULL, batch="batch",filter)
+{UseMethod("batch_correction")
+}
+
+#' @export
+batch_correction.ExpressionSet<-function(experiment, model,k=NULL,batch="batch",filter)
+  {
+    edata <- experiment@assayData$exprs
+    pheno <- experiment@phenoData
+    model.data<-model.frame(model, experiment@phenoData@data[all.vars(model)])
+    mod=model.matrix(model, data=model.data)
+
+
+data.limma <-  ExpressionSet(assayData = removeBatchEffect(edata, batch =  experiment[[batch]],
+                                                           design = mod), phenoData = experiment@phenoData)
+
+data.GFS <-  ExpressionSet(assayData = gfs(edata), phenoData = experiment@phenoData)
+
+
+data.quantile <-  normalize.quantiles.robust(edata,copy=TRUE,weights=NULL,remove.extreme="variance",
+                                             use.median=FALSE,use.log2=TRUE) # using the geometric mean
+colnames( data.quantile) <- colnames(edata)
+rownames( data.quantile ) <- rownames(edata)
+data.quantile <-  ExpressionSet(assayData = data.quantile, phenoData = experiment@phenoData)
+
+
+data.ComBat1 <-  ExpressionSet(assayData = ComBat(dat=edata, batch= experiment[[batch]], par.prior=TRUE, prior.plots=FALSE),
+                               phenoData = experiment@phenoData) #parametric adjustment
+data.ComBat2 <-  ExpressionSet(assayData = ComBat(dat=edata, batch= experiment[[batch]], par.prior=FALSE, mean.only=TRUE),
+                               phenoData = experiment@phenoData) #non-parametric adjustment, mean-only version
+data.Q_ComBat <-  ExpressionSet(assayData = ComBat(dat=exprs(data.quantile), batch= experiment[[batch]], par.prior=TRUE, prior.plots=FALSE),
+                               phenoData = experiment@phenoData) #parametric adjustment
+
+data.MNN <-  ExpressionSet(assayData = as.matrix(assay(mnnCorrect(edata, batch= experiment[[batch]]))), phenoData = experiment@phenoData)
+
+SV <- if(is.null(k))  {
+  num_sv     <- sva::num.sv(dat = edata,mod = mod, method = "be")
+  num_sv_l     <- sva::num.sv(dat = edata,mod = mod, method = "leek")
+  SV <- ifelse(num_sv < num_sv_l,num_sv,num_sv_l)
+} else {
+  SV=k
+}
+
+empi.controls <- empirical.controls(edata,mod,mod0=NULL,n.sv=SV,type="norm")
+genes <-  as.character(row.names(edata))
+control.genes <- Batchevaluation::control.genes
+
+control.genes <-     if(filter=='symbol'){
+  control.genes <- as.array(control.genes$hgnc_symbol)
+} else if(filter=='entrezgene_id'){
+  control.genes<- as.array(control.genes$entrezgene_id)
+} else {
+  control.genes <- as.array(control.genes$ensembl_gene_id)
+}
+ctrl.genes <- intersect(control.genes,genes)
+
+
+data.naiveRandRUV_HK <-  ExpressionSet(assayData = t(naiveRandRUV(t(edata),ctrl.genes, nu.coeff=0.0, k=SV, tol=1e-6)),
+                                       phenoData = experiment@phenoData)
+
+
+data.Q_naiveRandRUV_HK <-  ExpressionSet(assayData = t(naiveRandRUV(t(exprs(data.quantile)),ctrl.genes, nu.coeff=0, k=SV, tol=1e-6)),
+                                         phenoData = experiment@phenoData)
+
+
+data.naiveRandRUV_empi.controls <-  ExpressionSet(assayData = t(naiveRandRUV(t(edata),empi.controls, nu.coeff=0, k=SV, tol=1e-6)),
+                                                  phenoData = experiment@phenoData)
+
+
+data.Q_naiveRandRUV_empi.controls <-  ExpressionSet(assayData = t(naiveRandRUV(t(exprs(data.quantile)),empi.controls, nu.coeff=0.0, k=SV, tol=1e-6)),
+                                                    phenoData = experiment@phenoData)
+
+
+batchcorrected <- list()
+batchcorrected$data.limma <- data.limma
+batchcorrected$data.GFS <- data.GFS
+batchcorrected$data.quantile <- data.quantile
+batchcorrected$data.ComBat1 <- data.ComBat1
+batchcorrected$data.ComBat2 <- data.ComBat2
+batchcorrected$data.Q_ComBat<- data.Q_ComBat
+
+batchcorrected$data.mnncorrect <- data.MNN
+
+batchcorrected$data.Q_naiveRandRUV_HK <- data.Q_naiveRandRUV_HK
+batchcorrected$data.naiveRandRUV_HK <- data.naiveRandRUV_HK
+batchcorrected$data.naiveRandRUV_empi.controls <- data.naiveRandRUV_empi.controls
+batchcorrected$data.Q_naiveRandRUV_empi.controls <- data.Q_naiveRandRUV_empi.controls
+
+
+batchcorrected
+}
+
+
+#' @export
+batch_correction.SummarizedExperiment <-function(experiment, model, k=NULL, batch="batch")
+  {
+  log<-experiment@assays@data %>% names %>% switch(log_counts=TRUE, counts=FALSE)
+  edata <- experiment@assays@data[[1]]
+  edata <- edata[rowSums(edata)>0,]
+  model.data<-model.frame(model, experiment@colData[all.vars(model)])
+  mod=model.matrix(model, data=model.data)
+
+  data.limma <-  SummarizedExperiment(assays=list(removeBatchEffect(edata, batch= as.numeric(as.factor(experiment$batch)),
+                                                             design = mod)),
+                                      colData = experiment@colData,
+                                      metadata = experiment@metadata)
+
+  data.ComBat <-  SummarizedExperiment(assays=list(ComBat(edata,batch= as.numeric(as.factor(experiment$batch)))),
+                                      colData = experiment@colData,
+                                      metadata = experiment@metadata)
+
+  data.ComBat_seq <-  SummarizedExperiment(assays=list(ComBat_seq(edata,batch= as.numeric(as.factor(experiment$batch)))),
+                                       colData = experiment@colData,
+                                       metadata = experiment@metadata)
+
+  data.MNN <-  SummarizedExperiment(assays=list(mnnCorrect(edata, batch=as.numeric(as.factor(experiment$batch)), cos.norm.out = F)@assays@data$corrected),
+                                    colData = experiment@colData,
+                                    metadata = experiment@metadata)
+
+  SV <- if(is.null(k))  {
+    num_sv     <- sva::num.sv(dat = edata,mod = mod, method = "be")
+    num_sv_l     <- sva::num.sv(dat = edata,mod = mod, method = "leek")
+    SV <- ifelse(num_sv < num_sv_l,num_sv,num_sv_l)
+  } else {
+    SV=k
+  }
+
+
+  data.RUVs = SummarizedExperiment(assays =list(RUVs(edata, cIdx=seq_len(nrow(edata)), k=SV,
+                                                      scIdx=model.data %>% expand.grid %>% apply(1,paste) %>% makeGroups, isLog=TRUE)$normalizedCounts),
+                                    colData = experiment@colData,
+                                    metadata = experiment@metadata)
+
+
+
+  ccc <- QuantNorm(edata,as.numeric(as.factor(experiment$batch)),logdat=F,method='row/column',cor_method='pearson',max=5)
+
+  data.scBatch <-scBatchCpp(c=edata,d=ccc,m=5,w=diag(ncol(edata)),max=1200,tol=1e-10,step=0.0001,derif=scBatch::derif,verbose=F)
+  colnames(data.scBatch) = colnames(edata)
+  rownames(data.scBatch) = rownames(edata)
+
+  data.scBatch = SummarizedExperiment(assays =list(data.scBatch),
+                                   colData = experiment@colData,
+                                   metadata = experiment@metadata)
+
+ batchcorrected <- list()
+  batchcorrected$data.limma <- data.limma
+  batchcorrected$data.ComBat <- data.ComBat
+  batchcorrected$data.ComBat_seq <- data.ComBat_seq
+  batchcorrected$data.MNN <- data.MNN
+  batchcorrected$data.RUVs <- data.RUVs
+  batchcorrected$data.scBatch  <- data.scBatch
+  batchcorrected
+}
+
---
+++ R/Metadatacreation.R
@@ -0,0 +1,162 @@
+#' Loading of experiments in a list
+#' @description  Loading data in R- If the data files are already on your computer, you can use this step.
+#' @param directory A character string that is the path to the directory where all the experiments to load, and not any other file, are stored in Rdata format.
+#' @param item.SimpleList If the experiments are wrapped in a SimpleList object, this is the accessor (character string or integer) of the actual experiment object within this SimpleList. The experiment is unwrapped from this SimpleList.
+#' @return A list of the loaded experiments.
+#' @details  The experiment should be either bulk-RNAseq(Summarized experiment) or microarray(ExpressionSet).
+#'WARNING: Different types of experiment should not be processed together. For example, one Summarized experiment with expressionSet experiment, otherwise it will cause an error in further steps.
+#' @importFrom igraph graph_from_adjacency_matrix
+#' @importFrom igraph intersection
+#' @import stringr
+#' @import Biobase
+#' @import purrr
+#' @import magrittr
+#' @import ggplot2
+#' @import gtools
+#' @import readr
+#' @import Biobase
+#' @importFrom magrittr extract
+#' @importFrom utils download.file
+#' @import SummarizedExperiment
+#' @export
+load_experiments <- function(directory, names=dir(directory), item.SimpleList='rnaseq')
+{
+  directory %>% dir %>%
+    map(~get(load(paste0(directory,'/',.x)))) %>%
+    map(~if(class(.x)=='SimpleList') .x[[item.SimpleList]] else .x) %>%
+    set_names(names)
+}
+
+#' download_experiments_from_ExpressionAtlas
+#' @description  To use data from Expression Atlas that can be downloaded in `.Rdata` format.
+#' @param experiments Character list of Expression atlas experiment id
+#' @examples
+#' experiments <- download_experiments_from_ExpressionAtlas('E-ERAD-169','E-GEOD-73175','E-MTAB-2801')
+#' @return A list of the downloaded experiments.
+#' @details  The experiment should be either bulk-RNAseq(SummarizedExperiment) or microarray(ExpressionSet).
+#'WARNING: Different types of experiment should not be processed together. For example, one SummarizedExperiment with expressionSet experiment, otherwise it will cause an error in further steps.
+#' @export
+download_experiments_from_ExpressionAtlas<-function(..., destdir=getwd() %>% paste('experiments',sep='/')){
+  if(!(destdir %>% dir.exists) ){
+    destdir %>% dir.create
+  }
+  for(experiment in list(...)){
+    paste0('https://wwwdev.ebi.ac.uk/gxa/experiments-content/',experiment,'/static/',experiment,'-atlasExperimentSummary.Rdata') %>% download.file(destfile = paste0(destdir,"/",experiment,".Rdata"))
+  }
+  destdir %>% load_experiments
+}
+
+#' Removal of isolated experiments
+#' @description  To correct the batch effect, one needs to take the biological characteristics of the samples into account. If no sample of an experiment shares biological characteristics with samples from other batches, it is not possible to correct the batch effect with these batches since one cannot distinguish the biological difference from the artifact. The function remove_isolated_experiments removes the isolated experiments and plots graphs of intersections between the experiments before and after removal.
+#' @param experiments A list of wrapped experiments.
+#' @param  biological.group  A character string indicating the biological covariate that makes the biological groups. This must be the name of a column of the experiment.
+#' @example experiments %<>% remove_isolated_experiments('organism_part')
+#' @return A list of wrapped experiments.
+#' @details The experiments of the list that have no biological group in common with the other experiments are removed. This is an essential step to the integration of experiments using batch effect correction. Without it, the biological groups that are unshared between the experiments are confounding factors with the batch effect, and the latter cannot be corrected. The ultimate condition for this is the graph of biological intersections between the experiments to be a connected graph.
+#'
+#' WARNING: this function only checks and removes the experiments that are isolated. However, there can remain several clusters of experiments that are part of a connected subgraph, while the graph of all the experiments isn't connected. The graph of intersections after the removal of experiments is displayed so that the user can notice it. In this condition, the user has to choose manually some experiments that make a connected subgraph.
+#' @export
+remove_isolated_experiments<-function(experiments, biological.group){
+  warning('The following batches were removed as they do not have a ',biological.group,' column:\n',
+          names(experiments)[experiments %>% map(~is.null(.x[[biological.group]])) %>% unlist])
+  experiments[experiments %>% map(~is.null(.x[[biological.group]])) %>% unlist]<-NULL
+  batch<-experiments %>% imap(~.y %>% rep(dim(.x)[2])) %>% unlist(use.names=FALSE)
+  group<-experiments %>% map(~.[[biological.group]]) %>% unlist(use.names=FALSE)
+  groups <- group %>% split(batch)
+  intersections<-NULL
+  for(i in groups){
+    for(j in groups){
+      intersections%<>%c(length(intersect(i,j)))
+    }
+  }
+  intersections%<>%matrix(length(groups))%<>%set_colnames(names(groups))%<>%set_rownames(names(groups))
+  intersections %>% graph_from_adjacency_matrix %>% plot
+  experiments[rownames(intersections)[rowSums(intersections!=0)<=1]]<-NULL
+  message('The following batches were removed as they do not share any common point on ',biological.group,' column:\n',
+          rownames(intersections)[rowSums(intersections!=0)<=1])
+  batch<-experiments %>% imap(~.y %>% rep(dim(.x)[2])) %>% unlist(use.names=FALSE)
+  group<-experiments %>% map(~.[[biological.group]]) %>% unlist(use.names=FALSE)
+  groups <- group %>% split(batch)
+  intersections<-NULL
+  for(i in groups){
+    for(j in groups){
+      intersections%<>%c(length(intersect(i,j)))
+    }
+  }
+  intersections%<>%matrix(length(groups))%<>%set_colnames(names(groups))%<>%set_rownames(names(groups))
+  intersections %>% graph_from_adjacency_matrix %>% plot
+  return(experiments)
+}
+
+access_data<-function(experiment) UseMethod("access_data")
+access_data.SummarizedExperiment<-function(experiment){
+  return(experiment@assays$data$counts)
+}
+access_data.ExpressionSet<-function(experiment){
+  return(experiment@assayData$exprs)
+}
+
+access_pheno<-function(experiment) UseMethod("access_pheno")
+access_pheno.SummarizedExperiment<-function(experiment){
+  return(experiment@colData)
+}
+access_pheno.ExpressionSet<-function(experiment){
+  return(experiment@phenoData@data)
+}
+
+access_meta<-function(experiment) UseMethod("access_meta")
+access_meta.SummarizedExperiment<-function(experiment){
+  return(experiment@metadata)
+}
+access_meta.ExpressionSet<-function(experiment){
+  return(experiment@protocolData@data)
+}
+
+#' Rough merger of experiments
+#' @description  The function merge_experiments merges all the experiments in the list in a single experiment object and doesn't perform any correction. This function has two additional arguments log and filter (set to TRUE by default).
+#' It is recommended to give microarray data only with back-ground correction and after filtering low-expressed genes
+#' @param  experiments A list of wrapped experiments.
+#' @param  filter.unexpressed.genes A logical indicating if the genes that are unexpressed across all the samples of a batch shall be removed. This is for the proper functioning of the current version of ComBat in correct_batch_effect.
+#' @param log A logical ndicating of whether the data shall be log-transformed(recommended).
+#' @param force A logical. If the experiments of the list are of different classes, the function is stopped, unless force=TRUE.
+#' @example experiments %<>% merge_experiments(log=TRUE,filter=FALSE)
+#' @return Single SummarizedExperiment or ExpressionSet
+#' @note Caution: Sometime during merging experiments, phenodata (SDRF) file gets corrupted, it is advised to always check meta-data before proceeding further.
+
+#' @export
+merge_experiments <- function(experiments, filter.unexpressed.genes=TRUE, log, force=FALSE){
+  if(experiments %>% map(class) %>% unlist %>% unique %>% length %>% is_greater_than(1) & !force) stop("The experiments must have the same class. Their classes are :\n", experiments %>% map(class) %>% unlist)
+  genes<-experiments %>% map(rownames)
+  shared.genes<-genes %>% purrr::reduce(intersect)
+  unshared.genes<-genes %>% map(setdiff %>% partial(y=shared.genes))
+  data<-experiments %>% map(~.x %>% access_data %>% extract(shared.genes,)) %>% purrr::reduce(cbind)
+  warning(unshared.genes %>% unlist %>% unique %>% length,' genes have been removed as they are not shared across the batches.')
+  batch<-experiments %>% imap(~.y %>% rep(ncol(.x))) %>% unlist(use.names=FALSE) %>% factor
+  if(filter.unexpressed.genes){
+    unexpressed.genes <- data %>% t %>% data.frame %>% split(batch) %>% map(~colSums(.)==0) %>% purrr::reduce(`&`)
+    data%<>%extract(!unexpressed.genes,)
+    message(sum(unexpressed.genes),' genes have been removed as they were unexpressed across the samples of a batch.')
+  }
+  if(missing(log)){
+    log<-switch(
+      class(experiments[[1]]),
+      SummarizedExperiment=, RangedSummarizedExperiment=TRUE,
+      ExpressionSet=FALSE
+    )
+  }
+  if(log) data %<>% log1p
+  return(switch(
+    class(experiments[[1]]),
+    SummarizedExperiment=,RangedSummarizedExperiment=
+      SummarizedExperiment(
+        assays=if(log) list(log_counts=data) else list(counts=data),
+        colData=experiments %>% map(colData) %>% smartbind(list=.) %>% set_rownames(experiments %>% map(colnames) %>% unlist) %>% cbind(batch),
+        metadata=list(batch=batch)
+      ),
+    ExpressionSet=
+      ExpressionSet(
+        assayData=data,
+        phenoData=experiments %>% map(~.x@phenoData@data) %>% smartbind(list=.) %>% set_rownames(experiments %>% map(colnames) %>% unlist) %>% cbind(batch) %>% AnnotatedDataFrame
+      )
+  ))
+}
